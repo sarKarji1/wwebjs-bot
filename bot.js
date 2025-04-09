ce3f481b412a77c050eb51d74eac06b85249c0d0
Maurice Gift <ceo@giftedtech.web.id>
2025-04-09 23:58:21 +0300

    Gifted

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const fs = require('fs');
 const path = require('path');
-const { gmd, commands } = require('./lib/handler');
+const { gmd, commands } = require('./lib');
 
 const pluginsPath = path.join(__dirname, 'plugins');
 fs.readdirSync(pluginsPath).forEach((plugin) => {
